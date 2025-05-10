70de245ac43b7376a154feaf66976bf0e60e0c45
Leonardo <leodanielmp@gmail.com>
2025-05-09 20:26:10 -0600

    Initial Commit

---
+++ LearnOnline/LearnOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnOnlineApp.swift
+//  LearnOnline
+//
+//  Created by Mac OS lab on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
