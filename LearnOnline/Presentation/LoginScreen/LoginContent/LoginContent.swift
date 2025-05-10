59ef7ff7526773f29463d8ea74a7b01f63d55d90
Leonardo <leodanielmp@gmail.com>
2025-05-09 21:06:28 -0600

    Estructura de paquetes lista para pantallas, Creacion de la pantalla Login

---
+++ LearnOnline/Presentation/LoginScreen/LoginContent/LoginContent.swift
@@ -0,0 +1,8 @@
+//
+//  LoginContent.swift
+//  LearnOnline
+//
+//  Created by Mac OS lab on 09/05/25.
+//
+
+import Foundation
---
+++ LearnOnline/Presentation/LoginScreen/LoginScreen.swift
@@ -0,0 +1,8 @@
+//
+//  LoginScreen.swift
+//  LearnOnline
+//
+//  Created by Mac OS lab on 09/05/25.
+//
+
+import Foundation
